a90236c442ede4031b7453c06475f5db5c29b575
IgorDenisiuk <73896271+IgorDenisiuk@users.noreply.github.com>
2021-03-16 15:37:44 +0300

    Initial Commit

---
+++ Sunny/Supporting files/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  Sunny
+//
+//  Created by Igor Denisiuk on 16/03/2021.
+//  Copyright © 2021 Igor Denisiuk. All rights reserved.
+//
+
+import Foundation
+
+let apiKey = "256b3d3b3259fafaaff72d27d48430f6"
